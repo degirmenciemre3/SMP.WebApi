ac4eb763b5c0ee72d4a440b749c8583e9ccaa6ff
Emre Değirmenci <77126237+degirmenciemre3@users.noreply.github.com>
2024-01-26 19:07:54 +0300

    Delete SMA.WebApi directory

--- SMA.WebApi/Model/Student.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SMA.WebApi.Model;
-
-public class Student
-{
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string StudentNumber { get; set; }
-}
