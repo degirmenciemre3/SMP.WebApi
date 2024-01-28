b6f4f6467c5f732bec706e2b25d44ae164744e7b
Emre Değirmenci <degirmenciemre411@gmail.com>
2024-01-28 15:55:26 +0300

    Adding-CORS

--- SMA.WebApi/Program.cs
+++ SMA.WebApi/Program.cs
@@ -2,6 +2,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -17,6 +27,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// UseCors middleware'ini ekleyin
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
